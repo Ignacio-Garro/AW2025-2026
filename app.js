3cc9fbfdcf3eca2ae7cfb5693b78cde0dd2c2a68
Ignacio Garro <ignaciopapiro@gmail.com>
2025-11-20 11:26:04 +0100

    login

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-// 1. Importamos módulos
+//Importamos módulos
 const express = require('express');
 const path = require('path');
 const db = require('./config/db'); // Conexión MySQL
 
+const PORT = 3000;
 const app = express();
 
-// 2. Sirve TODOS los archivos de 'views' como estáticos
-// → HTML, CSS, partials (navbar, footer), JS, imágenes, etc.
+
+//Sirve TODOS los archivos de 'views' como estáticos
+//HTML, CSS, partials (navbar, footer), JS, imágenes, etc.
 app.use(express.static(path.join(__dirname, 'views')));
 
-// 2.1 Sirve TODO lo que esté en 'public' (imágenes, JS)
+//Sirve TODO lo que esté en 'public' (imágenes, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 3. Para leer formularios (POST)
+//Para leer formularios (POST)
 app.use(express.urlencoded({ extended: true }));
 
-// 4. Ruta principal → inicioSinLogin.html
+//Ruta principal → inicioSinLogin.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'inicioSinLogin.html'));
 });
 
-// 5. Puerto
-const PORT = 3000;
+//CODIGO DE LOGIN
 
-// 6. Iniciamos el servidor
+// Configurar EJS
+app.set('view engine', 'ejs'); // vamos a usar EJS como motor de plantillas
+
+//simulación de base de datos de usuarios - remplazar por base de datos real
+const usuarios = [
+  { email: 'ignacio@gmail.com', password: '123' },
+  { email: 'ana@gmail.com', password: 'password123' }
+];
+
+app.get('/login', (req, res) => { // cuando abre el login
+  res.render('login', { error: null, email: '' });
+});
+
+
+app.post('/login', (req, res) => { // cuando envía el formulario
+  const { email, password } = req.body;
+  const usuario = usuarios.find(u => u.email === email);
+
+  if(!usuario) {
+    res.render('login', { error: 'Usuario no encontrado', email });
+  } else if(usuario.password !== password) {
+    res.render('login', { error: 'Contraseña incorrecta', email });
+  } else {
+    res.redirect('/inicio.html'); // redirige a la página principal después del login
+  }
+});
+
+app.get('/inicio', (req, res) => {
+  res.send('¡Bienvenido a la página principal!');
+});
+
+
+
+
+//Iniciamos el servidor
 app.listen(PORT, () => {
   console.log(`Servidor en http://localhost:${PORT}`);
   console.log(`Abre: http://localhost:${PORT}`);
